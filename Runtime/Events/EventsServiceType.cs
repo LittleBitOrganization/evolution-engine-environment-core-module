85524c14aa7461c5fa91afc528a9e3da914c6dc9
plussamara <alligator85@list.ru>
2023-03-01 20:08:35 +0400

    fix EventServiceType

--- Runtime/Events/EventsServiceType.cs
+++ Runtime/Events/EventsServiceType.cs
@@ -8,8 +8,8 @@ namespace LittleBitGames.Environment.Events
         Firebase = 1 << 0,
         GA = 1 << 1,
         Adjust = 1 << 2,
-        Amplitude,
-        Wazzitude,
+        Amplitude = 1 << 3,
+        Wazzitude = 1 << 4,
         Everything = Int32.MaxValue,
         
     }
