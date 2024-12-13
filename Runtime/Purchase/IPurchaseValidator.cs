ee2843bbf9c0e15cf2564278c6dd54e8714b308e
TerentevVlad <ter.vlad.ter@gmail.com>
2024-12-13 13:06:06 +0300

    Добавил валидатор

---
+++ Runtime/Purchase/IPurchaseValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEditor.VersionControl;
+
+namespace LittleBitGames.Environment.Purchase
+{
+    public interface IPurchaseValidator
+    {
+        public event Action<string> OnSuccessValidate;
+        public event Action<string> OnFailureValidate;
+
+        public Task<bool> ValidateAsync();
+
+        public void Reset();
+    }
+}
